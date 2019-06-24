0ad4b59f7155d01b3af330b8dabecb43f3c089bc
gromley111 <gromley111@gmail.com>
2019-06-23 20:33:16 -0700

    first commit

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,47 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool endApp = false;
+            while (!endApp)
+            {
+                Console.Write("Please enter a word to check if it is a palindrome: ");
+                string str = Console.ReadLine();
+
+                //clean the string before inputing in palindrome checker
+                string FormattedString = FormatString(str);
+
+                bool IsPalindorme = PalindromeChecker(FormattedString);
+
+                Console.WriteLine(IsPalindorme);
+
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+            }
+        }
+
+        static bool PalindromeChecker(string str)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+
+            string newStr = string.Join("", arr);
+
+            if (newStr == str)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //incase user puts in a string with spaces or upper case letters
+        static string FormatString(string str)
+        {
+            string newStr = str.Replace(" ", "").ToLower();
+            return newStr;
         }
     }
 }
+
